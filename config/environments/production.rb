306135976ae96f90b7fe294b08c0cfac0ead0d68
pawanman56 <pawanman56@gmail.com>
2017-09-05 16:08:44 +0545

    Heroku production deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "OJT_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'ojt-challange.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
